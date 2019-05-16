cf9297f7ab3b5cb01f7ea75860229b80b0394a0b
tharanibaskaran <tharanibaskaran0508@gmail.com>
2019-05-16 11:54:19 +0530

    New one

--- src/GitNew1/BaseClass.java
+++ src/GitNew1/BaseClass.java
@@ -1,9 +1,10 @@
-package Sample;
+package GitNew1;
 
 public class BaseClass {
 
 	public static void main(String[] args) {
 		System.out.println("New Base Class");
+		System.out.println("New one");
 
 	}
 
